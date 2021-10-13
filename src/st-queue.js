debcb5124c1caa9289c5a71205229873ebdbc280
Katiarynka <katia.tsygankova@gmail.com>
2021-10-13 21:37:32 +0300

    feat: solve queue task

--- src/st-queue.js
+++ src/st-queue.js
@@ -13,21 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+ListNode = function(x) {
+    this.value = x;
+    this.next = null;
+}
+
 module.exports = class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    getUnderlyingList() {
+        return this.head;
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+    }
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    enqueue(value) {
+        // throw new NotImplementedError('Not implemented');
+        //  remove line with error and write your code here
+        if (this.length === 0) {
+            this.head = new ListNode(value);
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = new ListNode(value);
+        }
+        this.length++;
+    }
+
+    dequeue() {
+        //throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        if (this.length === 0) {
+            return undefined
+        }
+
+        let topElement = this.head.value;
+        this.head = this.head.next;
+        this.length--;
+        return topElement;
+    }
 
 }
